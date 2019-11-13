484ca67f8f3b8ac847a5618adf86ae80360e2bab
Sam Spilsbury <smspillaz@gmail.com>
2019-11-13 17:37:54 +0000

    benchmarks: Add connection scaling to branch_predictor

--- benchmarks/data-caching/benchmarks/intel/branch_predictor.sh
+++ benchmarks/data-caching/benchmarks/intel/branch_predictor.sh
@@ -7,4 +7,9 @@ for i in 64 128 256 512 1024 2048 4096 8192; do
   CLIENT_ARGS="-rps 180000 -k $i" bash server/run-perf-tests.sh
 done
 
+for i in 2 4 8 16 32 64; do
+  echo "Branch predictor, 4096 keys, $i conncetions"
+  CLIENT_ARGS="-rps 180000 -k 4096 -c $i" bash server/run-perf-tests.sh
+done
+
 
