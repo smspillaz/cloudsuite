b820c85dd98a6d6dc9c96810bb43da68100d90b8
Sam Spilsbury <smspillaz@gmail.com>
2019-11-19 12:04:54 -0500

    benchmarks: Bump number of keys and connections

--- benchmarks/data-caching/benchmarks/intel/branch_predictor.sh
+++ benchmarks/data-caching/benchmarks/intel/branch_predictor.sh
@@ -2,12 +2,12 @@
 export PERF_ARGS="-e branch-misses,branch-instructions,instructions"
 export SERVER_ARGS="-t 1 -m 4096 -n 550"
 
-for i in 64 128 256 512 1024 2048 4096 8192; do
+for i in 64 128 256 512 1024 2048 4096 8192 16384 32768 65536; do
   echo "Branch predictor, $i keys"
   CLIENT_ARGS="-rps 180000 -k $i" bash server/run-perf-tests.sh
 done
 
-for i in 2 4 8 16 32 64; do
+for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768; do
   echo "Branch predictor, 4096 keys, $i conncetions"
   CLIENT_ARGS="-rps 180000 -k 4096 -c $i" bash server/run-perf-tests.sh
 done
--- benchmarks/data-caching/benchmarks/intel/instructions-stack.sh
+++ benchmarks/data-caching/benchmarks/intel/instructions-stack.sh
@@ -2,7 +2,7 @@
 export PERF_ARGS="-e instructions:k,cycles:k,instructions:u,cycles:u"
 export SERVER_ARGS="-t 1 -m 4096 -n 550"
 
-for i in 2 4 8 16 32 64; do
+for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
   echo "Instructions Stack, 4096 keys, $i conncetions"
   CLIENT_ARGS="-rps 180000 -k 4096 -c $i" bash server/run-perf-tests.sh
 done
