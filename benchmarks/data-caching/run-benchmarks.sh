bfae5d052883cf9ca4ea45a806a2be280d310295
Sam Spilsbury <smspillaz@gmail.com>
2019-11-19 12:15:44 -0500

    benchmarks: Also log to file

--- benchmarks/data-caching/run-benchmarks.sh
+++ benchmarks/data-caching/run-benchmarks.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 ARCH=$1
+OUTPUT=$2
+
+mkdir -p $OUTPUT
 
 for i in $(ls benchmarks/$ARCH) ; do
     PATH=$(pwd)/benchmarks/$ARCH/$i;
-    bash ${PATH};
+    bash ${PATH} > $OUTPUT/${i}.${ARCH}.log;
 done
