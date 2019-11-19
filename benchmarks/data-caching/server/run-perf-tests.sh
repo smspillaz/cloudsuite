6ef007f75c61289d4abb2c66442a88e517941f5c
ZiluTian <zilu.tian@epfl.ch>
2019-11-19 12:08:51 -0500

    server: Use hardcoded perf path for perf tests
    This is necessary to bypass ubuntu's package from complaining about
    a kernel version that we can't actually install because the host is
    CentOS

--- benchmarks/data-caching/server/run-perf-tests.sh
+++ benchmarks/data-caching/server/run-perf-tests.sh
@@ -10,7 +10,7 @@ echo "Starting server with perf args ${PERF_ARGS} server args ${SERVER_ARGS}"
 docker rm -f dc-server || true;
 docker run --cpuset-cpus="0" -t --name dc-server --net caching_network \
     --cap-add SYS_ADMIN \
-    -e TOOL_ARGS="perf stat ${PERF_ARGS}" \
+    -e TOOL_ARGS="/usr/lib/linux-tools/4.15.0-66-generic/perf stat ${PERF_ARGS}" \ # Hardcoded to work around ubuntu package complaining
     -p 11211:11211 \
     -v $(pwd)/output:/output cloudsuite/data-caching:server bash docker-entrypoint.sh memcached ${SERVER_ARGS} &
 
