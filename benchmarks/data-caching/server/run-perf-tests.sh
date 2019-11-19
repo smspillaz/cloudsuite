bf5121abaf37148686e67deb0e020897bc8576a6
Sam Spilsbury <smspillaz@gmail.com>
2019-11-19 12:10:44 -0500

    server: Use smspillaz/cloudsuite:server|client as image names

--- benchmarks/data-caching/server/run-perf-tests.sh
+++ benchmarks/data-caching/server/run-perf-tests.sh
@@ -12,13 +12,13 @@ docker run --cpuset-cpus="0" -t --name dc-server --net caching_network \
     --cap-add SYS_ADMIN \
     -e TOOL_ARGS="/usr/lib/linux-tools/4.15.0-66-generic/perf stat ${PERF_ARGS}" \ # Hardcoded to work around ubuntu package complaining
     -p 11211:11211 \
-    -v $(pwd)/output:/output cloudsuite/data-caching:server bash docker-entrypoint.sh memcached ${SERVER_ARGS} &
+    -v $(pwd)/output:/output smspillaz/data-caching:server bash docker-entrypoint.sh memcached ${SERVER_ARGS} &
 
 echo "Starting client with ${CLIENT_ARGS}"
 
 docker rm -f cloudsuite-data-caching-perf-test-client || true;
 docker run --cpuset-cpus="1" -t --name cloudsuite-data-caching-perf-test-client --net caching_network \
-    cloudsuite/data-caching:client ${CLIENT_ARGS}
+    smspillaz/data-caching:client ${CLIENT_ARGS}
 
 echo "Done, sending SIGINT to memcached"
 MEMCACHED_PID="$(docker exec -it dc-server pidof memcached 2>&1)"
