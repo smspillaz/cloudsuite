1dffeb5ab4f9c19a547d959081649ddc30ec997b
Morten Borup Petersen <morten.petersen@epfl.ch>
2019-11-20 11:44:55 +0100

    Add Cavium plots

--- benchmarks/data-caching/benchmarks/logplotter.py
+++ benchmarks/data-caching/benchmarks/logplotter.py
@@ -56,12 +56,17 @@ class Benchmark:
 
 pp = pprint.PrettyPrinter(indent=4)
 
+
+def appendIfNotInlist(l, v):
+    if v not in l:
+        l.append(v)
+
 ################################################################################
 # User/Kernel instructions & Cycles
 
 # Intel x86 system 1    : x
 # Intel x86 system 2    : 
-# Arm Cavium            : 
+# Arm Cavium            : N/A                   No kernel instructions in logs
 # Amazon EC2 A1         : 
 
 # @todo: We need to compare this with total instruction count, to see how the
@@ -72,6 +77,7 @@ intel_InstrMix = Benchmark(
     ["instructions:k", "cycles:k", "instructions:u", "cycles:u"])
 intel_InstrMix.decodeLog(LOGDIR + "instructions-stack.sh.intel.log")
 
+############################### Data Extraction ################################
 connections = []
 user_i_ratio = []
 kernel_i_ratio = []
@@ -79,7 +85,7 @@ user_c_ratio = []
 kernel_c_ratio = []
 for k, v in intel_InstrMix.logVals['4096 keys'].items():
     if "conncetions" in k:
-        connections.append(k)
+        appendIfNotInlist(connections, k)
         kernel_i = v['instructions:k']
         user_i = v['instructions:u']
         kernel_c = v['cycles:k']
@@ -92,6 +98,7 @@ for k, v in intel_InstrMix.logVals['4096 keys'].items():
         user_c_ratio.append((user_c / c_tot) * 100)
         kernel_c_ratio.append((kernel_c / c_tot) * 100)
 
+################################### Plotting ###################################
 plt.subplot(2,2,1)
 plt.title("User versus Kernel instruction mix")
 plt.plot(user_i_ratio, label="User, Intel")
@@ -127,6 +134,10 @@ plt.xlabel("Keys")
 plt.legend()
 plt.show()
 
+
+
+
+
 ################################################################################
 # Branch predictor
 # Intel x86 system 1    : x
@@ -144,6 +155,8 @@ arm_cavium_branchPredictor = Benchmark(
     ["armv8_pmuv3_0/br_mis_pred/:u", "armv8_pmuv3_0/br_pred/:u", "instructions:u"])
 arm_cavium_branchPredictor.decodeLog(LOGDIR + "branch_predictor.sh.arm.cavium.log")
 
+
+############################### Data Extraction ################################
 # 4096 keys, varying connections
 # Branch misses
 plt.subplot(2,1,1)
@@ -152,7 +165,7 @@ x86_br_miss = []
 x86_br_mix = []
 for k, v in intel_branchPredictor.logVals['4096 keys'].items():
     if "conncetions" in k:
-        connections.append(k)
+        appendIfNotInlist(connections, k)
         missRatio = (v['branch-misses'] / v['branch-instructions']) * 100 
         brRatio = (v['branch-instructions'] / v['instructions']) * 100
         x86_br_miss.append(missRatio)
@@ -162,8 +175,7 @@ arm_cavium_br_miss = []
 arm_cavium_br_mix = []
 for k, v in arm_cavium_branchPredictor.logVals['4096 keys'].items():
     if "conncetions" in k:
-        if k not in connections:
-            connections.append(k)
+        appendIfNotInlist(connections, k)
         miss = v['armv8_pmuv3_0/br_mis_pred/:u']
         predict = v['armv8_pmuv3_0/br_pred/:u']
         total = miss + predict
@@ -175,6 +187,7 @@ for k, v in arm_cavium_branchPredictor.logVals['4096 keys'].items():
 # Just show the number of connections (Format of key is: "# connections")
 connections = [x.split(' ')[0] for x in connections]
 
+################################### Plotting ###################################
 plt.plot(x86_br_miss, label="x86")
 plt.plot(arm_cavium_br_miss, label="ARM (Cavium)")
 plt.title("Branch misses & branch instructions")
@@ -194,11 +207,14 @@ plt.legend()
 plt.show()
 
 
+
+
+
 ################################################################################
 # L1I Cache
 # Intel x86 system 1    : x
 # Intel x86 system 2    : 
-# Arm Cavium            :
+# Arm Cavium            : N/A                                   Needs a rerun
 # Amazon EC2 A1         : 
 ################################################################################
 intel_L1icache = Benchmark(
@@ -207,6 +223,7 @@ intel_L1icache = Benchmark(
 intel_L1icache.decodeLog(LOGDIR + "l1_cache.sh.intel.log")
 pp.pprint(intel_L1icache.logVals)
 
+############################### Data Extraction ################################
 connections = []
 hitrate = []
 for k, v in intel_L1icache.logVals.items():
@@ -214,6 +231,7 @@ for k, v in intel_L1icache.logVals.items():
         # Varying connections, stored as nested dictionaries
         for i, j in v.items():
             if(type(j) is dict):
+                appendIfNotInlist(connections, i.split()[0])
                 connections.append(i.split()[0]) # Just grab the number
                 hits = j['icache.hit']
                 misses = j['L1-icache-load-misses']
@@ -221,6 +239,7 @@ for k, v in intel_L1icache.logVals.items():
                 hitRatio = (hits / total) * 100 
                 hitrate.append(hitRatio)
 
+################################### Plotting ###################################
 # Varying connections
 plt.subplot(1,1,1)
 plt.plot(hitrate, label="x86")
@@ -232,44 +251,70 @@ plt.legend()
 
 plt.show()
 
-
 ################################################################################
 # L1D Cache
 # Intel x86 system 1    : x
 # Intel x86 system 2    : 
-# Arm Cavium            :
+# Arm Cavium            : x
 # Amazon EC2 A1         : 
 ################################################################################
 intel_L1dcache = Benchmark(
     "L1D cache",
     ["L1-dcache-load-misses", "L1-dcache-loads", "instructions"])
 intel_L1dcache.decodeLog(LOGDIR + "l1d_cache.sh.intel.log")
-pp.pprint(intel_L1dcache.logVals)
 
-plt.subplot(2,1,1)
+arm_cavium_L1dcache = Benchmark(
+    "L1D cache",
+    ["L1-dcache-load-misses:u", "L1-dcache-loads:u", "instructions"])
+arm_cavium_L1dcache.decodeLog(LOGDIR + "l1d_cache.sh.arm.cavium.log")
+
 keys = []
 connections = []
-l1d_hitrate_varyingkeys = []
-l1d_hitrate_4096_connections = []
-for k, v in intel_L1dcache.logVals.items():
+
+############################### Data Extraction ################################
+
+# Arm cavium
+arm_cavium_l1d_hitrate_varyingkeys = []
+arm_cavium_l1d_hitrate_4096_connections = []
+for k, v in arm_cavium_L1dcache.logVals.items():
     if "keys" in k:
         # Varying keys
-        keys.append(k.split()[0]) # Just grab the number
-        hitratio = (1 - (v['L1-dcache-load-misses'] / v['L1-dcache-loads'])) * 100 
-        l1d_hitrate_varyingkeys.append(hitratio)
+        appendIfNotInlist(keys, k.split()[0])
+        hitratio = (1 - (v['L1-dcache-load-misses:u'] / v['L1-dcache-loads:u'])) * 100 
+        arm_cavium_l1d_hitrate_varyingkeys.append(hitratio)
     
     if k == '4096 keys':
         # Varying connections, stored as nested dictionaries
         for i, j in v.items():
             if(type(j) is dict):
-                connections.append(i.split()[0]) # Just grab the number
-                hitratio = (1 - (j['L1-dcache-load-misses'] / j['L1-dcache-loads'])) * 100 
-                l1d_hitrate_4096_connections.append(hitratio)
+                appendIfNotInlist(connections, i.split()[0])
+                arm_cavium_l1d_hitrate_4096_connections.append(hitratio)
+                hitratio = (1 - (j['L1-dcache-load-misses:u'] / j['L1-dcache-loads:u'])) * 100 
 
+# x86
+x86_l1d_hitrate_varyingkeys = []
+x86_l1d_hitrate_4096_connections = []
+for k, v in intel_L1dcache.logVals.items():
+    if "keys" in k:
+        # Varying keys
+        appendIfNotInlist(keys, k.split()[0])
+        hitratio = (1 - (v['L1-dcache-load-misses'] / v['L1-dcache-loads'])) * 100 
+        x86_l1d_hitrate_varyingkeys.append(hitratio)
+    
+    if k == '4096 keys':
+        # Varying connections, stored as nested dictionaries
+        for i, j in v.items():
+            if(type(j) is dict):
+                appendIfNotInlist(connections, i.split()[0])
+                hitratio = (1 - (j['L1-dcache-load-misses'] / j['L1-dcache-loads'])) * 100 
+                x86_l1d_hitrate_4096_connections.append(hitratio)
+
+################################### Plotting ###################################
 # Varying key size
 plt.subplot(2,1,1)
 plt.title("L1D Properties with varying key and connection count")
-plt.plot(l1d_hitrate_varyingkeys, label="x86")
+plt.plot(x86_l1d_hitrate_varyingkeys, label="x86")
+plt.plot(arm_cavium_l1d_hitrate_varyingkeys, label="ARM (Cavium)")
 plt.xticks(range(len(keys)), keys)
 plt.ylabel("L1D Hit Ratio [%]")
 plt.xlabel("Keys")
@@ -277,7 +322,8 @@ plt.legend()
 
 # Varying connections
 plt.subplot(2,1,2)
-plt.plot(l1d_hitrate_4096_connections, label="x86")
+plt.plot(x86_l1d_hitrate_4096_connections, label="x86")
+plt.plot(arm_cavium_l1d_hitrate_4096_connections, label="ARM (Cavium)")
 plt.xticks(range(len(connections)), connections)
 plt.ylabel("L1D Hit Ratio [%]")
 plt.xlabel("Connections")
@@ -286,11 +332,71 @@ plt.legend()
 plt.show()
 
 
+
+
+
+################################################################################
+# L2D Cache
+# Intel x86 system 1    : 
+# Intel x86 system 2    : 
+# Arm Cavium            : x
+# Amazon EC2 A1         : 
+################################################################################
+
+arm_cavium_L2dcache = Benchmark(
+    "L2D cache",
+    ["armv8_pmuv3_0/l2d_cache/:u", "armv8_pmuv3_0/l2d_cache_refill/:u", "instructions"])
+arm_cavium_L2dcache.decodeLog(LOGDIR + "l2d_cache.sh.arm.cavium.log")
+
+############################### Data Extraction ################################
+keys = []
+connections = []
+arm_cavium_refillRatio_var_keys = []
+arm_cavium_refillRatio_var_conn = []
+for k, v in arm_cavium_L2dcache.logVals.items():
+    if "keys" in k:
+        # Varying keys
+        appendIfNotInlist(keys, k.split()[0])
+        ratio = (1 - (v['armv8_pmuv3_0/l2d_cache_refill/:u'] / v['armv8_pmuv3_0/l2d_cache/:u'])) * 100 
+        arm_cavium_refillRatio_var_keys.append(ratio)
+    
+    if k == '4096 keys':
+        # Varying connections, stored as nested dictionaries
+        for i, j in v.items():
+            if(type(j) is dict):
+                appendIfNotInlist(connections, i.split()[0])
+                ratio = (1 - (j['armv8_pmuv3_0/l2d_cache_refill/:u'] / j['armv8_pmuv3_0/l2d_cache/:u'])) * 100 
+                arm_cavium_refillRatio_var_conn.append(ratio)
+
+################################### Plotting ###################################
+# Varying key size
+plt.subplot(2,1,1)
+plt.title("L2D Refill ratio")
+plt.plot(arm_cavium_refillRatio_var_keys, label="Arm (Cavium)")
+plt.xticks(range(len(keys)), keys)
+plt.ylabel("L2D Refill Ratio [%]")
+plt.xlabel("Keys")
+plt.legend()
+
+# Varying connections
+plt.subplot(2,1,2)
+plt.plot(arm_cavium_refillRatio_var_conn, label="Arm (Cavium)")
+plt.xticks(range(len(connections)), connections)
+plt.ylabel("LLC Refill Ratio [%]")
+plt.xlabel("Connections")
+plt.legend()
+
+plt.show()
+
+
+
+
+
 ################################################################################
 # LLC Cache
 # Intel x86 system 1    : x
 # Intel x86 system 2    : 
-# Arm Cavium            :
+# Arm Cavium            : N/A                   No performance counters for LLC
 # Amazon EC2 A1         : 
 ################################################################################
 intel_LLCcache = Benchmark(
@@ -299,6 +405,8 @@ intel_LLCcache = Benchmark(
 intel_LLCcache.decodeLog(LOGDIR + "llc.sh.intel.log")
 pp.pprint(intel_LLCcache.logVals)
 
+
+############################### Data Extraction ################################
 keys = []
 connections = []
 VarKeysHitRatio = []
@@ -306,7 +414,7 @@ VarConnHitRatio = []
 for k, v in intel_LLCcache.logVals.items():
     if "keys" in k:
         # Varying keys
-        keys.append(k.split()[0]) # Just grab the number
+        appendIfNotInlist(keys, k.split()[0])
         hitRatio = (1 - (v['LLC-load-misses'] / v['LLC-loads'])) * 100 
         VarKeysHitRatio.append(hitRatio)
     
@@ -315,10 +423,11 @@ for k, v in intel_LLCcache.logVals.items():
         # Varying connections, stored as nested dictionaries
         for i, j in v.items():
             if(type(j) is dict):
-                connections.append(i.split()[0]) # Just grab the number
+                appendIfNotInlist(connections, i.split()[0])
                 hitRatio = (1 - (j['LLC-load-misses'] / j['LLC-loads'])) * 100 
                 VarConnHitRatio.append(hitRatio)
 
+################################### Plotting ###################################
 # Varying key size
 plt.subplot(2,1,1)
 plt.title("LLC Properties with varying key and connection count")
