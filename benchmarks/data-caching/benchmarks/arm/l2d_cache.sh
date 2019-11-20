2d33c465e18f704931ff544d1ad4816d6c8f9b33
Morten Borup Petersen <morten.petersen@epfl.ch>
2019-11-20 11:44:40 +0100

    Fix typo in L2D cache benchmark

--- benchmarks/data-caching/benchmarks/arm/l2d_cache.sh
+++ benchmarks/data-caching/benchmarks/arm/l2d_cache.sh
@@ -3,12 +3,12 @@ export PERF_ARGS="-e armv8_pmuv3_0/l2d_cache/,armv8_pmuv3_0/l2d_cache_wb/,armv8_
 export SERVER_ARGS="-t 1 -m 4096 -n 550"
 
 for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
-  echo "L1D cache, $i keys"
+  echo "L2D cache, $i keys"
   CLIENT_ARGS="-rps 180000 -k $i" bash server/run-perf-tests.sh
 done
 
 for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
-  echo "L1D cache, 4096 keys, $i conncetions"
+  echo "L2D cache, 4096 keys, $i conncetions"
   CLIENT_ARGS="-rps 180000 -k 4096 -c $i" bash server/run-perf-tests.sh
 done
 
