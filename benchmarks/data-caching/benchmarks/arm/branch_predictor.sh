0889ebe6637741db4bdbdffa4e9957c0dbb52edd
Sam Spilsbury <smspillaz@gmail.com>
2019-11-20 16:49:40 +0100

    benchmarks: Use fixed key size of 512, allow up to $i ** 2 open connections
    We need this so that our benchmarks don't fail

--- benchmarks/data-caching/benchmarks/arm/branch_predictor.sh
+++ benchmarks/data-caching/benchmarks/arm/branch_predictor.sh
@@ -4,12 +4,13 @@ export SERVER_ARGS="-t 1 -m 4096 -n 550"
 
 for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
   echo "Branch predictor, $i keys"
-  CLIENT_ARGS="-rps 180000 -k $i" bash server/run-perf-tests.sh
+  CLIENT_ARGS="-rps 180000 -k $i -f 512" bash server/run-perf-tests.sh
 done
 
 for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
   echo "Branch predictor, 4096 keys, $i conncetions"
-  CLIENT_ARGS="-rps 180000 -k 4096 -c $i" bash server/run-perf-tests.sh
+  max_conns=( $i**2 )
+  CLIENT_ARGS="-rps 180000 -k 4096 -c $i -f 512" SERVER_ARGS="$SERVER_ARGS -c $max_conns" bash server/run-perf-tests.sh
 done
 
 
--- benchmarks/data-caching/benchmarks/intel/branch_predictor.sh
+++ benchmarks/data-caching/benchmarks/intel/branch_predictor.sh
@@ -9,7 +9,8 @@ done
 
 for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768; do
   echo "Branch predictor, 4096 keys, $i conncetions"
-  CLIENT_ARGS="-rps 180000 -k 4096 -c $i" bash server/run-perf-tests.sh
+  max_conns=( $i**2 )
+  CLIENT_ARGS="-rps 180000 -k 4096 -c $i -f 512" SERVER_ARGS="$SERVER_ARGS -c $max_conns" bash server/run-perf-tests.sh
 done
 
 
