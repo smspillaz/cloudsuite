774ad946ae0307facb5dbf88439bd91eddb26654
Sam Spilsbury <smspillaz@gmail.com>
2019-11-21 08:42:05 +0100

    benchmarks: Bump max_conns by 128
    Just to allow for a few more open files

--- benchmarks/data-caching/benchmarks/arm/l2d_cache.sh
+++ benchmarks/data-caching/benchmarks/arm/l2d_cache.sh
@@ -9,7 +9,7 @@ done
 
 for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384; do
   echo "L2D cache, 4096 keys, $i conncetions"
-  max_conns=( $i**2 )
+  max_conns=$(echo "$i + 128" | bc)
   CLIENT_ARGS="-rps 180000 -k 4096 -c $i -f 512" SERVER_ARGS="$SERVER_ARGS -c $max_conns" bash server/run-perf-tests.sh
 done
 
