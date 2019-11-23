f1e21a96b33ff6533e63320b2ba8f6746dabdde2
Morten Borup Petersen <morten_bp@live.dk>
2019-11-23 11:10:18 +0100

    Add system specifier to benchmark execution script

--- benchmarks/data-caching/run-benchmarks.sh
+++ benchmarks/data-caching/run-benchmarks.sh
@@ -3,11 +3,12 @@
 set -x
 
 ARCH=$1
-OUTPUT=$2
+SYS=$2
+OUTPUT=$3
 
 mkdir -p $OUTPUT
 
 for i in $(ls benchmarks/$ARCH) ; do
     path=$(pwd)/benchmarks/$ARCH/$i;
-    bash ${path} > $OUTPUT/${i}.${ARCH}.log;
+    bash ${path} > $OUTPUT/${i}.${ARCH}.${SYS}.log;
 done
